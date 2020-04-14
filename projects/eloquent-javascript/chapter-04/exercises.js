4001654cc91a7ead7cbd81594f9271584da864a7
Ubuntu <ubuntu@ip-172-31-45-116.ec2.internal>
2020-04-14 00:09:39 +0000

    pseudo code update

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -110,10 +110,29 @@ function arrayToList(array) {
   //I'll need to loop through the array.
   //Will i need a constructor function to build the objects?
   let list = {};
-  let x
+  let x;
+  let listSoFar;
   for (let i = array.length-1; i >= 0; i--) {
     //add object to list
-    //list = {value: X, rest: list}
+   if(i === array.length-1) {
+    x = array[i];
+    // console.log('x='+x)
+    //local binding for list so far
+    listSoFar = list;
+    // console.log('listSoFar='+listSoFar)
+    list = {value: x, rest: null}
+    // console.log('list='+list)
+     
+   } else {
+    x = array[i];
+    // console.log('x='+x)
+    //local binding for list so far
+    listSoFar = list;
+    // console.log('listSoFar='+listSoFar)
+    list = {value: x, rest: list}
+    // console.log('list='+list)
+   }
+    
     
     //So to begin, make an object
     //value is array[i] and rest: null
@@ -121,52 +140,190 @@ function arrayToList(array) {
     //assign the rest value and assign value array[i]
     
   }
+  return list;
 }
-
 ////////////////////////////////////////////////////////////////////////////////
 // listToArray /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function listToArray() {
-  //I-
-  //o-
+function listToArray(list) {
+  //I-list
+  //o-array
   //C-
   //E-
+  let array = [];
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value)
+  }
+  return array;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // prepend /////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function prepend() {
-  //I-
-  //o-
+function prepend(element, list) {
+  //I-element, and list
+  //o-return the list with the element added to the top of the list
   //C-
   //E-
+  //Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front 
+  //of the input list.
+  let oldList = list;
+  list = {
+    value: element,
+    rest: oldList
+  }
+  return list;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // nth /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function nth() {
-  //I-
-  //o-
+function nth(list, number) {
+  //I-list and a number
+  //o-returns the element at the given position in the list
   //C-
-  //E-
+  //E- undefined when no element
+  //and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
+
+//If you haven’t already, also write a recursive version of nth.
+
+//The recursive version of nth will, similarly, look at an ever smaller part of the “tail” of the list and at the same time count down the 
+//index until it reaches zero, at which point it can return the value property of the node it is looking at. To get the zeroth element of a 
+//list, you simply take the value property of its head node. To get element N + 1, you take the Nth element of the list that’s in this list’s 
+//rest property.
+  let counter = -1
+  for (let node = list; node; node = node.rest) {
+    counter += 1;
+    if (number === counter) {
+      return node.value;
+    }
+  }
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // deepEqual ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+var log = console.log
+function deepEqual(thing1, thing2) {
+  log('start of function')
+  log('thing1='+thing1)
+  log('thing2='+thing2)
+  //First check if object
+  if(typeof(thing1) === 'object') {
+    log('type of thing1= '+ typeof(thing1) +', start of if chain 216')
+    //Initiate for in loop
+    for(var key in thing1) {
+      //check if value is an object
+      if(typeof(thing1[key] === 'object')) {
+        log('type of thing1 key= '+typeof(thing1[key])+', start of if chain 222')
+        //if object, recurse
+        log('RECURSION')
+        return deepEqual(thing1[key], thing2[key])
+      }
+      //check num of keys/props
+      if(Object.keys(thing1).length !== Object.keys(thing2).length) {
+        log('object lenghts unequal fail 228')
+        return false
+      }
+      //This is where I do the array comparison. It's getting into the next line and bunging it up.
+      log('thing1[key] =' + thing1[key])
+      //It's a full object with a nested array. why hasn't it recursed?
+      //it's not, its an array. ok. that's better. it's [1, 2, 3]
+      log(typeof(thing1[key]))
+      log(Array.isArray(thing1[key]))
+      if(typeof(thing1[key])== 'object' && Array.isArray(thing1[key])) {
+        //array check with true or false returns
+        //loop?
+        log('test')
+        for(let i = 0; i <thing1[key].length; i++) {
+          log('3rdtest')
+          log(thing1[key][i])
+          log(thing2[key][i])
+          if (thing1[key][i] !=thing2[key][i]) {
+            log('2ndtest')
+            return false
+          }
+        }
+        log(thing1)
+        log('why not here?');
+        return true;
+       
 
-function deepEqual() {
-  //I-
-  //o-
-  //C-
-  //E-
+      }
+
+      //compare values
+      if(thing1[key] !== thing2[key]) {
+        log('thing1 key not equal thing2 key, '+thing1[key] + ' / '+thing2[key] +' /fail line 233')
+        return false
+      }
+      
+    }
+    //Null/Undefined Finder (needs to be before empty finder)
+    if (typeof thing1 == "object" && thing2 == null) {
+      return false
+    }
+    //losing my nested null here
+    //I'll try doing an if check to filter it?
+    log(thing1)
+    if(thing1 == null && thing2 != null) {
+      log('here we are')
+      return false
+    }
+
+
+
+    
+    //Empty Object Finder
+    if(Object.keys(thing1).length ==0 &&Object.keys(thing2).length==0) {
+      return true;
+    }
+  }
+  //compare values
+  //should I have an object exempter here?
+  //If this is still here, I left it in
+  //appears to be working.
+  if(typeof thing1 !== 'object') {
+  if (thing1 !== thing2) {
+    log('thing1 ='+thing1)
+    log('thing2 =' +thing2)
+    // if(thing1 ==='' && thing2 ==='') {
+    //   return true
+    // }
+    log('failing at 242')
+    return false
+  }
+  }
+    
+        //if object, recurse
+      //check if nested arrays?
+      //check num of keys/props
+      //compare keys
+      //compare values
+  //compare values
+  //if objects recursive call
+  //once pass return true
+  return true;
 }
+console.log(deepEqual({},{}))
 
+//check length of keys
+//check keys
+//check values
+//if object cycle back
+
+//Things to test for
+  //2 empty objects
+  //2 numbers
+  //2 falsey values
+  //2 objects same props and values
+  //2 objects same props and diff values
+  //2 objects identical nested arrays
+  //2 objects unique nested arrays
+  //2 objects with nested objects
 ////////////////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -2,33 +2,181 @@
 // flatten /////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function flatten() {
+function flatten(args) {
+  //Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.
+  //I-arrays
+  //O-single sleek array
+  //C-
+  //E-
+  //use concat and reduce
+  //create result array
+  //search down to lowest array level
+  //reduce with concat to make the new array
+  
+  function grab(value, nextValue) {
+    let output = []
+    output = value.concat(nextValue)
+    return output
+  }
 
+  //need to go to next level of arrays. 
+  
+  
+//   for(let i =0; i<args.length; i++) {
+//     if(Array.isArray(args[i])==true) {
+//     flatten(args[i])
+//   }
+// }
+let result = [];
+result = args.reduce(grab)
+
+  // for(let i = 0; i < args.length; i++) {
+  //   result = args[i].reduce(grab)
+  //}
+
+  // let result = args.reduce(grab)
+  return result
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // loop ////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
-
-function loop() {
-
+function loop(value, testFun, updateFun, bodyFun) {
+while(testFun(value)) {
+    bodyFun(value);
+    value = updateFun(value);
+  }
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // every ///////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function every() {
-
+function every(array, test) {
+  //I-array and test
+  //O-true if every element passes, false if any fail
+  //C-
+  //E-
+  for(let i = 0; i <array.length;i++) {
+    if(test(array[i]) == false) {
+      return false
+    }
+  }
+  return true;
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function dominantDirection() {
+function dominantDirection(text) {
+  
+  
 
+  //I-text
+  //O-determine the dominate direction in a string of text, and output that
+  //C-
+  //E-
+  
+  //use characterscript and countby
+  
+  //count characters
+  //filter out non-helpful ones
+ 
+  //
+  let scripts = countBy(text, char=> {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({direction}) => direction != "none");
+console.log(scripts, 'scripts')
+
+  let highest = {
+    count: -1
+  };
+  for (let i = 0; i< scripts.length; i++){
+    if (scripts[i].count > highest.count && scripts[i].name != 'none'){
+      highest = scripts[i];
+    }
+  }
+  return highest.name;
+
+
+  let total = scripts.reduce((n, {count}) => n + count, 0);
+  if (total == 0) return "No scripts found";
+console.log(total)
+  scripts.reduce((preR, element)=>{
+    
+  },[])
+//filter out none's
+//console.log(SCRIPTS.filter(s => s.direction == "ttb"));
+console.log(scripts.filter(name => name.direction != 'none'))
+
+//reduce to find answer
+  function characterCount(script) {
+  return script.ranges.reduce((count, [from, to]) => {
+    return count + (to - from);
+  }, 0);
 }
+}
+
+  
+   
+  
+// function characterScript(code) {
+//   for (let script of SCRIPTS) {
+//     if (script.ranges.some(([from, to]) => {
+//       return code >= from && code < to;
+//     })) {
+//       return script;
+//     }
+//   }
+//   return null;
+// }
+
+// console.log(characterScript(121));
+
+// // → {name: "Latin", …}function countBy(items, groupName) {
+//   let counts = [];
+//   for (let item of items) {
+//     let name = groupName(item);
+//     let known = counts.findIndex(c => c.name == name);
+//     if (known == -1) {
+//       counts.push({name, count: 1});
+//     } else {
+//       counts[known].count++;
+//     }
+//   }
+//   return counts;
+// }
+
+// console.log(countBy([1, 2, 3, 4, 5], n => n > 2));
+// // → [{name: false, count: 2}, {name: true, count: 3}]
+// 
+
+// function textScripts(text) {
+//   let scripts = countBy(text, char => {
+//     let script = characterScript(char.codePointAt(0));
+//     return script ? script.name : "none";
+//   }).filter(({name}) => name != "none");
+
+//   let total = scripts.reduce((n, {count}) => n + count, 0);
+//   if (total == 0) return "No scripts found";
+
+//   return scripts.map(({name, count}) => {
+//     return `${Math.round(count * 100 / total)}% ${name}`;
+//   }).join(", ");
+// }
+
+// console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
+
+// → 61% Han, 22% Latin, 17% Cyrillic
+
+//Your solution might look a lot like the first half of the textScripts example. You again have to count characters by a criterion 
+//based on characterScript and then filter out the part of the result that refers to uninteresting (script-less) characters.
+
+//Finding the direction with the highest character count can be done with reduce. If it’s not clear how, refer to the example 
+//earlier in the chapter, where reduce was used to find the script with the most characters.
+
 
 // /////////////////////////////////////////////////////////////////////////////
 //  //////////////////////////////////////////////////////
